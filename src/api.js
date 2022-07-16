43c45eceae1e5d148447cc5bd8b8c795a56c9f19
AlexKomanov <akomanov88@gmail.com>
2022-07-16 12:37:59 +0300

    Added api.js file

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c21c494448msh0e7a9b4c01014a6p15a0ebjsndaeaf31d15b5',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
