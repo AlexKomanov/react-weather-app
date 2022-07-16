b3a9ccc80e53ff28ab57ddd508a805fc2ab38c72
AlexKomanov <akomanov88@gmail.com>
2022-07-16 14:57:58 +0300

    Changes in api.js file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoAPIOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'c21c494448msh0e7a9b4c01014a6p15a0ebjsndaeaf31d15b5',
+        'X-RapidAPI-Key': '', // Enter the X-RapidAPI-Key here
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
